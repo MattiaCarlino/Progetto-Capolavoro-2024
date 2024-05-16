3a13e676b76246d711c2fc3d441cdfb114d68166
Mattia Carlino <mattia.carlino.mc06@gmail.com>
2024-05-16 21:00:09 +0200

    commit BUG Topics_page and Topics

--- progetto_capolavoro_2024/src/components/Topics_page/Topics_page.jsx
+++ progetto_capolavoro_2024/src/components/Topics_page/Topics_page.jsx
@@ -6,15 +6,12 @@ export const Topics_page = ({topics}) => {
         <div className='list_sotto_topics'>
             <h6>{topics.list_sotto_topics}</h6>
         </div>
-        <section className='content-section'>
-            <div className='title_topics'>
-                <h2 >{topics.list_sotto_topics.title_sotto_topics}</h2>
-            </div>
-            <div className='content'>
-                {topics.list_sotto_topics.content}
-            </div>
-        </section>
-
+        <div className='title_topics'>
+            <h2 >{topics.list_sotto_topics.title_sotto_topics}</h2>
+        </div>
+         <div className='content'>
+            {topics.list_sotto_topics.content}
+        </div>
     </div>
   )
 }
--- progetto_capolavoro_2024/src/pages/Home/Home.jsx
+++ progetto_capolavoro_2024/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React,useState} from 'react' 
 import Topic_box from '../../components/Topics/Topics.jsx'
 import list_topics from '../../list_topics.json'
 import Topics_page  from '../../components/Topics_page/Topics_page.jsx'
@@ -7,35 +7,39 @@ import './Home.css'
 
 export const Home = () => {
 
-  const renderTopicPage = () =>{
-    {list_topics.map(topics => (
-      <Topics_page
-      key = {topics.id}
-      topics = {topics}
-      />
-    ))}
-  
-  }
+  const [isClicked,setClicked] = useState(false);
+  const [idDaVisualizzare, setIDDaVisualizzare] = useState(0);
 
   return (
     <div id = "homepage" className='wrapper2'>
-      <div className='web-app-name'><h1> Nome web app </h1></div>
+    <div className='web-app-name'><h1> Nome web app </h1></div>
     <div className='description'>
       <p> descrizione del progetto, come è diviso e organizzato</p>
     </div>
     <div className='title-main-section'><h1> Argomenti </h1></div>
-    <section className='main-section'>
+    {!isClicked && (<section className='main-section'>
       <div className='left-section'>
       {list_topics.map(topics => (
           <Topic_box
             key={topics.id}
             topics={topics}
-            click = {renderTopicPage}
+            click = {setClicked(true)}
           />
         ))}
       </div>
-    </section>
+    </section>)}
 
+    {isClicked &&(
+      <section>
+        <div>
+        {list_topics.map(topics => (
+        <Topics_page
+        key = {topics.id}
+        topics = {topics}
+        />
+        ))}
+        </div>
+      </section>)}
     </div>
   )
 }
