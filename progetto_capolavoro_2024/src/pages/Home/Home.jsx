287aa9df5970bd3a2a82fcbb9437eb5e9d414021
Mattia Carlino <mattia.carlino.mc06@gmail.com>
2024-05-17 12:34:07 +0200

    sistemazione changes

--- progetto_capolavoro_2024/src/pages/Home/Home.jsx
+++ progetto_capolavoro_2024/src/pages/Home/Home.jsx
@@ -2,8 +2,7 @@ import {React,useState} from 'react'
 import Topic_box from '../../components/Topics/Topics.jsx'
 import list_topics from '../../list_topics.json'
 import Topics_page  from '../../components/Topics_page/Topics_page.jsx'
-
-
+import './Home.css'
 
 export const Home = () => {
 
@@ -12,36 +11,42 @@ export const Home = () => {
 
   return (
     <div id = "homepage" className='wrapper2'>
-    <div className='web-app-name'><h1> Nome web app </h1></div>
-    <div className='description'>
+      <div className='web-app-name'><h1> Nome web app </h1></div>
+      <div className='description'>
       <p> descrizione del progetto, come è diviso e organizzato</p>
-    </div>
-    <div className='title-main-section'><h1> Argomenti </h1></div>
-    {!isClicked && (<section className='main-section'>
-      <div className='left-section'>
-      {list_topics.map(topics => (
+      </div>
+      <div className='title-main-section'><h1> Argomenti </h1></div>
+      {!isClicked && (<section className='main-section'>
+        <div className='left-section'>
+          {list_topics.map(topics => (
           <Topic_box
             key={topics.id}
             topics={topics}
             click = {setClicked(true)}
           />
-        ))}
-      </div>
-    </section>)}
+          ))}
+        </div>
+      </section>)}
 
     {isClicked &&(
       <section>
-        <div>
-        {list_topics.map(topics => (
-        <Topics_page
-        key = {topics.id}
-        topics = {topics}
-        />
-        ))}
-        </div>
+        <h1> FERRI MI HA ROTTO IL CODICE GRAZIE FERRI : / </h1>
+
       </section>)}
     </div>
-  )
+    )
 }
 
 export default Home;
+
+/*<section>
+<div>
+{list_topics.map(topics => (
+<Topics_page
+key = {topics.id}
+topics = {topics}
+/>
+))}
+</div>
+</section>
+*/
