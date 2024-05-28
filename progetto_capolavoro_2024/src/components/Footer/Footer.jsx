8b81a13089afaee670129bc3e586e3cbca670448
davideferri2006@gmail.com <davideferri2006@gmail.com>
2024-05-28 17:30:21 +0200

    stile home concluso - o quasi - mo si pensa al funzionamento: manca 2^ pagina e About Us

--- progetto_capolavoro_2024/src/components/Footer/Footer.jsx
+++ progetto_capolavoro_2024/src/components/Footer/Footer.jsx
@@ -9,13 +9,15 @@ export const Footer = () => {
     <div class="container">
         <div class="footer-content">
             <div class="footer-logo">
-                <img src="https://img.freepik.com/premium-photo/cyber-digital-world-android-robot-pointing-finger-computer-coding-ai-chatbot-background_629685-1221.jpg" alt="Logo"></img>
+                <a href='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.isarchimede.edu.it/&ved=2ahUKEwj5kIS4z7CGAxX7gP0HHUi8DeoQFnoECAYQAQ&usg=AOvVaw1mmBG6DM-2L1eVCzcFwoKT' target='_blank'>
+                <img src="https://scontent.fmxp12-1.fna.fbcdn.net/v/t39.30808-6/302138691_464954835647691_3659711233732093474_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ON6HVvovZPkQ7kNvgHFcQtd&_nc_ht=scontent.fmxp12-1.fna&oh=00_AYA5eGdbgjk7FS1b-IO-HV6TqMUS7DTfBer0io5K74U0-A&oe=665BC954" alt="Logo" id='archimede'></img>
+                </a>
             </div>
             <div class="footer-links">
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li> | </li>
-                    <li><Link to="/About_us">Contatti</Link></li>
+                    <li><Link to="/About_us">About us</Link></li>
                 </ul>
             </div>
             
@@ -23,9 +25,26 @@ export const Footer = () => {
         <div class="footer-info">
             <p>© 2024 ITIS Archimede</p>
             <p>Indirizzo: Via Greggia, 52, 24047 Treviglio BG</p>
-            <p>Email: ferri.davide.stu@isarchimede.it | barboni.luca.stu@isarchimede.it |   carlino.mattia.stu@isarchimede.it
+            <p>Email: <a href='https://mail.google.com/mail/u/0/?pli=1#inbox?compose=CllgCJlLWRklPbhSZbrdvdMmhWqmvqBtJmvMPBwnRWkRfrCZhQfdGgbJjfpZVCpwtFbgcGGMcvq'target='_blank'>ferri.davide.stu@isarchimede.it</a> | <a href='https://mail.google.com/mail/u/0/?pli=1#inbox?compose=CllgCJlLWRklPbhSZbrdvdMmhWqmvqBtJmvMPBwnRWkRfrCZhQfdGgbJjfpZVCpwtFbgcGGMcvq'target='_blank'>carlino.mattia.stu@isarchimede.it</a>
             </p>
         </div>
+
+        <div>
+
+        <p id='titolo'> Utilities: </p>
+
+        <nav>
+            <ul id='utilities'>
+                
+                <li> <a href='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://it.legacy.reactjs.org/&ved=2ahUKEwj9hsab0rCGAxWcgv0HHQOgCbUQFnoECBwQAQ&usg=AOvVaw1qFG6icnSO7LRQkh8Hcs4j'target='_blank'>React</a></li>
+                <li> <a href='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://unica.istruzione.gov.it/it&ved=2ahUKEwiOxail0rCGAxU-i_0HHYM4AtQQFnoECAYQAQ&usg=AOvVaw1vD0O1DmVL-x96OusjGHMF'target='_blank'>Unica</a></li>
+                <li> <a href='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.isarchimede.edu.it/&ved=2ahUKEwj5kIS4z7CGAxX7gP0HHUi8DeoQFnoECAYQAQ&usg=AOvVaw1mmBG6DM-2L1eVCzcFwoKT' target='_blank'>ITIS Archimede Treviglio</a></li>
+            </ul>
+        </nav>
+
+
+        </div>
+
     </div>
 </footer>
 
